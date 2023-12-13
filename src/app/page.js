94181a16116688ba1a99e7cf4976096c7914e761
joselinares17 <josedaniellinares135@gmail.com>
2023-12-13 16:26:22 -0500

    Modifying entity file

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Layout from "../components/Layout"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Entitys from "@/components/Entitys"
 import { Container } from "react-bootstrap"
 
 const Page = () => {
@@ -12,7 +13,7 @@ const Page = () => {
       () => <div>
         <h4>ChongApi</h4>
         <Container style={{ alignContent: "center", marginTop: "2%" }}>
-
+          <Entitys/>
         </Container>
       </div>
     }
--- src/components/Entitys.js
+++ src/components/Entitys.js
@@ -1,14 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import data from '../json/locations.json'
+
 
 const Entitys = () => {
 return (
     <Container>
         <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
+            {data.map(item => (
+                <Col key = {item.id}>
+                    <h3>{item.name}</h3>
+                    <p>{item.capacity}</p>
+                    <p>{item.description}</p>
+                </Col>
+            ))}
         </Row>
     </Container>
 );

54ca56a1777218854ad3b9708ef6c00172808339
joselinares17 <josedaniellinares135@gmail.com>
2023-12-13 16:49:21 -0500

    Modifying entitys file

--- src/components/Entitys.js
+++ src/components/Entitys.js
@@ -4,40 +4,25 @@ import Col from 'react-bootstrap/Col';
 import { Card } from 'react-bootstrap';
 import data from '../json/locations.json'
 
-
 const Entitys = () => {
-  return (
-    <Container>
-      <Row>
-        {data.map(item => (
-          /**
-          <Col key={item.id}>
-            <h3>{item.name}</h3>
-            <p>{item.capacity}</p>
-            <p>{item.description}</p>
-          </Col>
-          **/
-          <Col>
-            <Card
-              key={item.id}
-              bg="dark"
-              text="light"
-              style={{ width: '18rem' }}
-              className="mb-2"
-            >
-              <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
-              <Card.Body>
-                <Card.Title>{item.capacity}</Card.Title>
-                <Card.Text>
-                  {item.description}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-}
+    return (
+        <Container>
+        <Row>
+            {data.map(item => (
+            <Col>
+                <Card key={item.id} bg="dark" text="light" style={{ width: '18rem' }} className="mb-2">
+                <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
+                <Card.Body>
+                    <Card.Title>Capacity: {item.capacity}</Card.Title>
+                    <Card.Text>{item.location}</Card.Text>
+                    <Card.Text>{item.description}</Card.Text>
+                </Card.Body>
+                </Card>
+            </Col>
+            ))}
+        </Row>
+        </Container>
+    );
+    }
 
 export default Entitys

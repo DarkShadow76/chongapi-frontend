93ffb4fb8c05ef65ec5720ff274215a82fa2f299
DarkShadow76 <alonzotoricaceres@gmail.com>
2023-12-13 16:41:31 -0500

    Estilo cards

--- src/components/Entitys.js
+++ src/components/Entitys.js
@@ -1,23 +1,43 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Card } from 'react-bootstrap';
 import data from '../json/locations.json'
 
 
 const Entitys = () => {
-return (
+  return (
     <Container>
-        <Row>
-            {data.map(item => (
-                <Col key = {item.id}>
-                    <h3>{item.name}</h3>
-                    <p>{item.capacity}</p>
-                    <p>{item.description}</p>
-                </Col>
-            ))}
-        </Row>
+      <Row>
+        {data.map(item => (
+          /**
+          <Col key={item.id}>
+            <h3>{item.name}</h3>
+            <p>{item.capacity}</p>
+            <p>{item.description}</p>
+          </Col>
+          **/
+          <Col>
+            <Card
+              key={item.id}
+              bg="dark"
+              text="light"
+              style={{ width: '18rem' }}
+              className="mb-2"
+            >
+              <Card.Header style={{ textAlign: 'center' }}>{item.name}</Card.Header>
+              <Card.Body>
+                <Card.Title>{item.capacity}</Card.Title>
+                <Card.Text>
+                  {item.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
-);
+  );
 }
 
 export default Entitys

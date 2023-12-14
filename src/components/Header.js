e971df67b36fcec6b8df752392d00d352a8e8b78
DarkShadow76 <alonzotoricaceres@gmail.com>
2023-12-13 20:56:28 -0500

    Fix: Navbar satic test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
 const Header = (props) => {
+  const [activeSection, setActiveSection] = useState("home");
+
+  const handleNavClick = (section) => {
+    setActiveSection(section);
+    //alert(`${section}`)
+  };
+
   return (
     <>
-      <Navbar expand="sm" className="bg-body-tertiary Navbar" bg="dark" variant="dark" fixed="top">
+      <Navbar
+        expand="sm"
+        className={`bg-body-tertiary Navbar ${activeSection === "home" ? "home-active" : ""}`}
+        bg="dark"
+        variant="dark"
+        fixed="top">
         <Container className="container-nav">
           <Navbar.Brand href="/">ChongApi</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="#" className="nav-item active" disabled>Support
+              <Nav.Link href="/" onClick={() => handleNavClick("home")}>Home</Nav.Link>
+              <Nav.Link href="/about" onClick={() => handleNavClick("about")}>About</Nav.Link>
+              <Nav.Link href="/support" onClick={() => handleNavClick("support")}>Support
               </Nav.Link>
-              <Nav.Link href="#">Review
+              <Nav.Link href="/review" onClick={() => handleNavClick("review")}>Review
               </Nav.Link>
               <NavDropdown title="Servicios" id="nav-dropdown" menuVariant="dark">
                 <NavDropdown.Item href="#action3" >

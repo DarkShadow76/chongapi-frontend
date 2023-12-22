5977f9a016d93351f48ff84d1b2a9051209c9458
razor <josedaniellinares135@gmail.com>
2023-12-21 21:31:27 -0500

    adding tailwind config and files

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,6 +42,7 @@ export default function RootLayout({ children }) {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       />
+      <link href="/dist/output.css" rel="stylesheet"/>
       <body className={style.body}>
         <NavBar/>
         {children}

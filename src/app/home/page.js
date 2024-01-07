55914678bd06424e0b0098c039dee14203bdc3a3
DarkShadow76 <alonzotoricaceres@gmail.com>
2024-01-07 08:51:04 -0500

    Landing Page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,20 +2,25 @@ import { Image, Col, Row, Container } from "react-bootstrap";
 
 const Page = () => {
   return (
-    <div>
-      <h3>Infórmate de los mejores puntos para divertite</h3>
-      <h5>Más de 100 negocios en todo la costa confían en nosotros.</h5>
-      {/*<Image className='image' src="https://peru21.pe/resizer/rW0neezFs8wc0QM9JUHIZXf-DQI=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/YY5FXW5DLRCYRMJOIT54J4AL3M.jpg" thumbnail />*/}
-      <h3>ChongAPI es tu guia para el mundo Chongueril</h3>
-      {/*<Image className="image" src="https://cdn.www.gob.pe/uploads/document/file/3212701/standard_15.jpg.jpg" rounded /> */}
-      {/*Imagen grande */}
-      {/*Mostrar 3 beneficios de la página icono, titulo <h3>, descripcion <p>*/}
-      {/*Mostra los socios que tenemos "logo del local" */}
-      <h6>
-        ChongApi la mejor plataforma para brindarte información sobre tus
-        anfitrionas
-      </h6>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={12} md={6}>
+          <h3>Infórmate de los mejores puntos para divertite</h3>
+          <h5>Más de 100 negocios en todo la costa confían en nosotros.</h5>
+          <Image className='image' src="https://peru21.pe/resizer/rW0neezFs8wc0QM9JUHIZXf-DQI=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/YY5FXW5DLRCYRMJOIT54J4AL3M.jpg" thumbnail />
+        </Col>
+        <Col xs={12} md={6} style={{ marginBottom: "5rem" }}>
+          <h3>ChongAPI es tu guia para el mundo Chongueril</h3>
+          <Image className="image" src="https://cdn.www.gob.pe/uploads/document/file/3212701/standard_15.jpg.jpg" rounded />
+        </Col>
+        {/*Imagen grande */}
+        {/*Mostrar 3 beneficios de la página icono, titulo <h3>, descripcion <p>*/}
+        {/*Mostra los socios que tenemos "logo del local" */}
+        <Col>
+          <h6>ChongApi la mejor plataforma para brindarte información sobre tus anfitrionas</h6>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
